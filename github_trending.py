04bd335a7ca707eef60df6f35bd0707d91a65889
Vladimir <smirnovv91@gmail.com>
2018-03-07 13:02:14 +0300

    add base functions

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,34 @@
+import requests
+from datetime import date, timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+def time_delta(days=7):
+    date_days_ago = date.today() - timedelta(days)
+    return date.isoformat(date_days_ago)
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_trending_repositories(date):
+    response = requests.get(
+        r"https://api.github.com/search/"
+        r"repositories?q=created:>{date}&sort=stars&order=desc".format(
+            date=date
+        )
+    )
+    return response.json()
 
-if __name__ == '__main__':
-    pass
+
+if __name__ == "__main__":
+    count_repositories = 20
+    trending_repositories_dict = get_trending_repositories(time_delta(days=7))
+    trending_repositories_list = trending_repositories_dict["items"]
+    for number, repo  in enumerate(trending_repositories_list):
+        if number == count_repositories:
+            break
+        stargazers_count = repo["stargazers_count"]
+        open_issues_count = repo["open_issues_count"]
+        repo_url = repo["html_url"]
+        print(
+            "Stars:", stargazers_count,
+            "Open issues:", open_issues_count,
+            "URL:", repo_url
+        )
